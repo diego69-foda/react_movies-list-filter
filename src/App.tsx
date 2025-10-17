507aa8dfb8d0d800615b150114ce488e8d145ea1
diego69-foda <diego.bkuhn@gmail.com>
2025-10-17 09:46:04 -0300

    feat: implement movie search functionality in App component

--- src/App.tsx
+++ src/App.tsx
@@ -4,12 +4,17 @@ import { MoviesList } from './components/MoviesList';
 import moviesFromServer from './api/movies.json';
 
 export const App: React.FC = () => {
+  const [query, setQuery] = React.useState('');
+
+  const filteredMovies = moviesFromServer.filter(movie =>
+    movie.title.toLowerCase().includes(query.toLowerCase())
+  );
+
   return (
     <div className="page">
       <div className="page-content">
         <div className="box">
           <div className="field">
-            {/* eslint-disable-next-line jsx-a11y/label-has-associated-control */}
             <label htmlFor="search-query" className="label">
               Search movie
             </label>
@@ -20,12 +25,14 @@ export const App: React.FC = () => {
                 id="search-query"
                 className="input"
                 placeholder="Type search word"
+                value={query}
+                onChange={(event) => setQuery(event.target.value)}
               />
             </div>
           </div>
         </div>
 
-        <MoviesList movies={moviesFromServer} />
+        <MoviesList movies={filteredMovies} />
       </div>
 
       <div className="sidebar">Sidebar goes here</div>
